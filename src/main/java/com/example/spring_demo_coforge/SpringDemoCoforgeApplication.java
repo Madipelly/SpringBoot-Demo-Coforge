f481c2f1775d73caf807c5bfb4b5170792badd6c
madipelly <shravan.madipelly@gmail.com>
2025-02-03 21:33:57 +0530

    first commit

---
+++ src/main/java/com/example/spring_demo_coforge/SpringDemoCoforgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo_coforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoCoforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoCoforgeApplication.class, args);
+	}
+
+}
